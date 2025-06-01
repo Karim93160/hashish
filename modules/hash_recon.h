b3057afe03fda6e6454d4f39586a6e4d7c50fd8b
Karim <gallotiankarim@gmail.com>
2025-06-01 23:01:29 +0200

    Create hash_recon.h

---
+++ modules/hash_recon.h
@@ -0,0 +1,10 @@
+#ifndef HASH_RECON_H
+#define HASH_RECON_H
+
+#include <string>
+
+// Déclaration de la fonction analyzeHash
+// Elle prend une chaîne de caractères (le hachage) et affiche une suggestion.
+void analyzeHash(const std::string& hash);
+
+#endif // HASH_RECON_H
