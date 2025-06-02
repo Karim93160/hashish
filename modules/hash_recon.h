be781a2bed3ff543aafa29555a06e5d51d952d1f
Karim <gallotiankarim@gmail.com>
2025-06-02 02:47:05 +0200

    Update hash_recon.h
    Nettoyage

--- modules/hash_recon.h
+++ modules/hash_recon.h
@@ -1,15 +1,12 @@
 #ifndef HASH_RECON_H
 #define HASH_RECON_H
 
-#include <string> // Nécessaire car les fonctions utilisent std::string
+#include <string>
 
-// Déclarations des fonctions de vérification de type de caractères
-// Elles retournent true si la chaîne est composée uniquement du type spécifié, false sinon.
 bool estNumerique(const std::string& str);
 bool estAlphabetique(const std::string& str);
 bool estAlphanumerique(const std::string& str);
 bool estHexadecimal(const std::string& str);
-// La fonction d'analyse a le nom français ici
-void analyserTypeCaracteresHachage(const std::string& hash_hex); 
+void analyserTypeCaracteresHachage(const std::string& hash_hex);
 
 #endif // HASH_RECON_H
